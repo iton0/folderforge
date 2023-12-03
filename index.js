b195f47c40744502423577c31b8c62f4aa37624a
iton0 <iton442@gmail.com>
2023-12-03 15:14:19 -0500

    refactor: Update event listeners
    - Deleted forge.js and moved code into index.js

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
       newContainer.appendChild(nestItems);
     }
     dragContainer.appendChild(newContainer);
+    undoStack.push(newContainer);
 
     // Nesting Logic Functions
     function updateNestingLevel(dropTargetNestingLevel, draggedItem) {
@@ -322,10 +323,56 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  function createFileSystem(zip, $parent, basePath = "") {
+    const folderName = $parent
+      .querySelector(".folder-name")
+      .getAttribute("name");
+    const folderPath =
+      basePath === "" ? folderName : `${basePath}/${folderName}`;
+
+    zip.folder(folderPath);
+
+    const children = Array.from($parent.querySelector(".children").children);
+    children.forEach((child) => {
+      if (child.getAttribute("class") === "file") {
+        const fileName = child.querySelector(".file-name").getAttribute("name");
+        zip.file(folderPath + "/" + fileName, "");
+      } else {
+        const childFolderName = child
+          .querySelector(".folder-name")
+          .getAttribute("name");
+        zip.folder(folderPath + "/" + childFolderName);
+        createFileSystem(zip, child, folderPath);
+      }
+    });
+  }
+
   document.addEventListener("click", (e) => {
     const target = e.target;
     if (target.id === "clear") {
       handleClear();
+    }
+    if (target.id === "forge") {
+      const dragContainer = document.getElementById("drag-container");
+      const rootElement = dragContainer.querySelector(".folder");
+      const zip = new JSZip();
+
+      if (!rootElement) {
+        alert("Root folder is missing");
+      } else if (rootElement.querySelector(".children").children.length < 2) {
+        alert("Root folder must have at least two elements");
+      } else {
+        const rootName = rootElement
+          .querySelector(".folder-name")
+          .getAttribute("name");
+
+        createFileSystem(zip, rootElement);
+
+        zip.generateAsync({ type: "blob" }).then((blob) => {
+          const zipName = `${rootName}.zip`;
+          saveAs(blob, zipName);
+        });
+      }
     }
   });
 });
